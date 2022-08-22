ef0a1e175c7b61d6c1d054457f7d3d8c01614fac
Ritel Takajun <little_down@outlook.com>
2022-08-22 16:24:06 +0800

    Delete assets directory

--- assets/autoload.js
+++
@@ -1,8 +0,0 @@
-$('body').append('<div class="waifu" id="waifu" style="font-size: 40%;"><div class="waifu-tips" id="waifu-tips"></div><canvas class="live2d" id="live2d"></canvas><div class="waifu-tool" id="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-close"></span></div></div>');
-
-$.ajax({ url: 'assets/waifu-tips.js?v=1.4.2', dataType: "script", cache: true, async: false });
-$.ajax({ url: 'assets/live2d.min.js?v=1.0.5', dataType: "script", cache: true, async: false });
-
-// live2d_settings 在 waifu-tips.js 中修改
-
-initModel('assets/waifu-tips.json');
